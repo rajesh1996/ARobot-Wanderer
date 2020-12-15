c36f17a8357b7e3a7b3e19a1db7cf7438c5f3028
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-12-15 00:37:46 -0500

    [Task 2.13]Adding test cases for navigate plan collect tasks

---
+++ test/testSpawnCollect.cpp
@@ -0,0 +1,71 @@
+/*****************************************************************************************
+ Copyright (C) 2020 Arjun Srinivasan Ambalam,Rajeshwar N.S
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+   * Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************************/
+
+/**
+ *  @copyright MIT License 2020 Arjun Srinivasan Ambalam,Rajeshwar N.S
+ *  @file    Navigatebot.hpp
+ *  @author  Arjun Srinivasan Ambalam
+ *  @author  Rajeshwar N.S
+ *  @date    12/07/2020
+ *  @version 1.0
+ *
+ *  @brief Final Project - wanderer-bot (Search and collect objects)
+ *
+ *  @section DESCRIPTION
+ *  Test cases for pathPlanner Algorithm
+ */
+
+#include <gtest/gtest.h>
+#include "../include/SpawnCollect.hpp"
+
+/**
+ * @def TEST(testspawnCollect, checkSpawn)
+ * @brief To check if trash is spawned
+ */
+TEST(testspawnCollect, checkSpawn) {
+SpawnCollect testn;
+// should be true on spawn
+EXPECT_TRUE(testn.spawn(8.367830, 0.2700, 0.9203, 1));
+}
+
+/**
+ * @def TEST(testspawnCollect, checkCollect)
+ * @brief To check if trash is collected
+ */
+TEST(testspawnCollect, checkCollect) {
+SpawnCollect testn;
+// should be true on collect
+EXPECT_TRUE(testn.collect(0));
+}
+
+/**
+ *  @brief  Main Function for running tests
+ *  @param  int argc, char argv
+ *  @return int
+ */
+int main(int argc, char **argv) {
+testing::InitGoogleTest(&argc, argv);
+return RUN_ALL_TESTS();
+}
